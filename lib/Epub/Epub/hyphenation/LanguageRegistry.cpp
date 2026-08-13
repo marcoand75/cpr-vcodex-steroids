d96809b84c0290bdd45cc354462d296ac2e1b75b
Marco Andreacchio <marcoand75@gmail.com>
2026-08-13 15:32:31 +0200

    feat(epub): make non-core hyphenation dictionaries opt-in
    Wrap Spanish/French/Polish/Russian/Swedish/Ukrainian (and German) Liang
    hyphenation trie data behind CPR_ENABLE_*_HYPHENATION build flags, defaulting
    them off to reduce flash. English/Italian/Portuguese remain enabled.
    
    Books in disabled languages still render; hyphenation falls back to explicit
    hyphens/fallback points. Default build drops from 99.9% to 98.2% flash.

--- lib/Epub/Epub/hyphenation/LanguageRegistry.cpp
+++ lib/Epub/Epub/hyphenation/LanguageRegistry.cpp
@@ -4,48 +4,84 @@
 
 #include "HyphenationCommon.h"
 #include "generated/hyph-en.trie.h"
-#include "generated/hyph-es.trie.h"
-#include "generated/hyph-fr.trie.h"
 #include "generated/hyph-it.trie.h"
-#include "generated/hyph-pl.trie.h"
 #include "generated/hyph-pt.trie.h"
-#include "generated/hyph-ru.trie.h"
-#include "generated/hyph-sv.trie.h"
-#include "generated/hyph-uk.trie.h"
 
 #if CPR_ENABLE_GERMAN_HYPHENATION
 #include "generated/hyph-de.trie.h"
+#endif
+#if CPR_ENABLE_SPANISH_HYPHENATION
+#include "generated/hyph-es.trie.h"
+#endif
+#if CPR_ENABLE_FRENCH_HYPHENATION
+#include "generated/hyph-fr.trie.h"
+#endif
+#if CPR_ENABLE_POLISH_HYPHENATION
+#include "generated/hyph-pl.trie.h"
+#endif
+#if CPR_ENABLE_RUSSIAN_HYPHENATION
+#include "generated/hyph-ru.trie.h"
+#endif
+#if CPR_ENABLE_SWEDISH_HYPHENATION
+#include "generated/hyph-sv.trie.h"
+#endif
+#if CPR_ENABLE_UKRAINIAN_HYPHENATION
+#include "generated/hyph-uk.trie.h"
 #endif
 
 namespace {
 
 // English hyphenation patterns (3/3 minimum prefix/suffix length)
 LanguageHyphenator englishHyphenator(en_patterns, isLatinLetter, toLowerLatin, 3, 3);
+#if CPR_ENABLE_FRENCH_HYPHENATION
 LanguageHyphenator frenchHyphenator(fr_patterns, isLatinLetter, toLowerLatin);
+#endif
 #if CPR_ENABLE_GERMAN_HYPHENATION
 LanguageHyphenator germanHyphenator(de_patterns, isLatinLetter, toLowerLatin);
 #endif
+#if CPR_ENABLE_RUSSIAN_HYPHENATION
 LanguageHyphenator russianHyphenator(ru_patterns, isCyrillicLetter, toLowerCyrillic);
+#endif
+#if CPR_ENABLE_SPANISH_HYPHENATION
 LanguageHyphenator spanishHyphenator(es_patterns, isLatinLetter, toLowerLatin);
+#endif
 LanguageHyphenator italianHyphenator(it_patterns, isLatinLetter, toLowerLatin);
+#if CPR_ENABLE_POLISH_HYPHENATION
 LanguageHyphenator polishHyphenator(pl_patterns, isLatinLetter, toLowerLatin);
+#endif
 LanguageHyphenator portugueseHyphenator(pt_patterns, isLatinLetter, toLowerLatin);
+#if CPR_ENABLE_SWEDISH_HYPHENATION
 LanguageHyphenator swedishHyphenator(sv_patterns, isLatinLetter, toLowerLatin);
+#endif
+#if CPR_ENABLE_UKRAINIAN_HYPHENATION
 LanguageHyphenator ukrainianHyphenator(uk_patterns, isCyrillicLetter, toLowerCyrillic);
+#endif
 
 static const LanguageEntry kEntries[] = {
     {"english", "en", &englishHyphenator},
+#if CPR_ENABLE_FRENCH_HYPHENATION
     {"french", "fr", &frenchHyphenator},
+#endif
 #if CPR_ENABLE_GERMAN_HYPHENATION
     {"german", "de", &germanHyphenator},
 #endif
+#if CPR_ENABLE_RUSSIAN_HYPHENATION
     {"russian", "ru", &russianHyphenator},
+#endif
+#if CPR_ENABLE_SPANISH_HYPHENATION
     {"spanish", "es", &spanishHyphenator},
+#endif
     {"italian", "it", &italianHyphenator},
+#if CPR_ENABLE_POLISH_HYPHENATION
     {"polish", "pl", &polishHyphenator},
+#endif
     {"portuguese", "pt", &portugueseHyphenator},
+#if CPR_ENABLE_SWEDISH_HYPHENATION
     {"swedish", "sv", &swedishHyphenator},
+#endif
+#if CPR_ENABLE_UKRAINIAN_HYPHENATION
     {"ukrainian", "uk", &ukrainianHyphenator},
+#endif
 };
 
 }  // namespace
