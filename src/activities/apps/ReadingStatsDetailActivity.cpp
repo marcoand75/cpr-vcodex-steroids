b1b40826e8b235d1cc145424a3c802ecb3c34da7
Marco Andreacchio <marcoand75@gmail.com>
2026-08-31 20:34:28 +0200

    refactor: collapse conditional hint pair in ReadingStatsDetailActivity
    The BACK/conditional confirmLabel/UP/DOWN mappedInput.mapLabels +
    GUI.drawButtonHints pair collapses to ListRenderHelper::drawHints.
    The conditional confirmLabel (SELECT or OPEN or "") is preserved.

--- src/activities/apps/ReadingStatsDetailActivity.cpp
+++ src/activities/apps/ReadingStatsDetailActivity.cpp
@@ -23,6 +23,7 @@
 #include "components/UITheme.h"
 #include "components/icons/settings2.h"
 #include "fontIds.h"
+#include "../util/ListRenderHelper.h"
 #include "util/HeaderDateUtils.h"
 #include "util/ReadingStatsAnalytics.h"
 #include "util/TimeUtils.h"
@@ -744,8 +745,7 @@ void ReadingStatsDetailActivity::render(RenderLock&&) {
   }
 
   const char* confirmLabel = actionsSelected ? tr(STR_SELECT) : (Storage.exists(bookPath.c_str()) ? tr(STR_OPEN) : "");
-  const auto labels = mappedInput.mapLabels(tr(STR_BACK), confirmLabel, tr(STR_DIR_UP), tr(STR_DIR_DOWN));
-  GUI.drawButtonHints(renderer, labels.btn1, labels.btn2, labels.btn3, labels.btn4);
+  ListRenderHelper::drawHints(renderer, mappedInput, tr(STR_BACK), confirmLabel, tr(STR_DIR_UP), tr(STR_DIR_DOWN));
 
   renderer.displayBuffer();
 }
